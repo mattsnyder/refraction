d2c6da1bbdc0ce39e55ff3874149fdaccc43f087
Matt Snyder <snyder2112@me.com>
2013-07-18 10:55:38 -0400

    Setup Rails app and basic gemset

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Refraction::Application.config.secret_key_base = 'a6dd049cda7ddee64992158e988a93c446bf54c47cae60a5c9cb74cf8f8286bacf88c387a717c96e5441bdc9e9cf165e8f4541bdf18c40f18d54f8ced0b8a48b'
